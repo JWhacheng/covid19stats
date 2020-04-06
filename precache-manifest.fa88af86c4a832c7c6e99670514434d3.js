cf9927c7ecc8d09b4bdef075a2e7017fa8d1a0b7
Joseph Whacheng <josephwhacheng98@gmail.com>
2020-04-06 15:01:03 -0600

    Updates

---
+++ precache-manifest.fa88af86c4a832c7c6e99670514434d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cac83628c08507255f277ba595d692c",
+    "url": "/covid19stats/index.html"
+  },
+  {
+    "revision": "01fec7bd9911284559d2",
+    "url": "/covid19stats/static/css/main.942ee303.chunk.css"
+  },
+  {
+    "revision": "68078a286ad488ca8625",
+    "url": "/covid19stats/static/js/2.c104ccdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19stats/static/js/2.c104ccdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01fec7bd9911284559d2",
+    "url": "/covid19stats/static/js/main.3bb38b68.chunk.js"
+  },
+  {
+    "revision": "0a8ded1ca794007cb682",
+    "url": "/covid19stats/static/js/runtime-main.bcc9b402.js"
+  }
+]);
